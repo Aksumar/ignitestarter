5b7c066bfab4886d67a8c365733e06b946810dbe
Исхакова Лилия Ринатовна <liliya.iskhakova@moex.com>
2020-02-11 14:03:26 +0300

    Стартер почищен от дополнительных настроек, которые использовались для тестирования

--- src/main/java/com/esb/IgniteStarter/IgniteSpringBootStarterConfiguration.java
+++ src/main/java/com/esb/IgniteStarter/IgniteSpringBootStarterConfiguration.java
@@ -1,20 +1,13 @@
 package com.esb.IgniteStarter;
 
 import org.apache.ignite.Ignite;
-import org.apache.ignite.IgniteEvents;
 import org.apache.ignite.IgniteLogger;
 import org.apache.ignite.Ignition;
 import org.apache.ignite.configuration.IgniteConfiguration;
-import org.apache.ignite.events.DiscoveryEvent;
-import org.apache.ignite.events.EventType;
-import org.apache.ignite.lang.IgnitePredicate;
 import org.apache.ignite.logger.slf4j.Slf4jLogger;
-import org.apache.ignite.plugin.segmentation.SegmentationPolicy;
 import org.apache.ignite.spi.communication.tcp.TcpCommunicationSpi;
 import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
 import org.apache.ignite.spi.discovery.tcp.ipfinder.vm.TcpDiscoveryVmIpFinder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,8 +15,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableConfigurationProperties({IgniteProperties.class})
 public class IgniteSpringBootStarterConfiguration {
-
-    Logger logger = LoggerFactory.getLogger(IgniteSpringBootStarterConfiguration.class);
 
     @Bean
     public Ignite ignite(IgniteProperties clientProperties) {
@@ -50,23 +41,11 @@ public class IgniteSpringBootStarterConfiguration {
         discoverySpi.setIpFinder(ipFinder);
 
         IgniteLogger log = new Slf4jLogger();
-
         igniteConfiguration.setGridLogger(log);
 
-        igniteConfiguration.setSegmentationPolicy(SegmentationPolicy.NOOP);
         igniteConfiguration.setDiscoverySpi(discoverySpi);
         igniteConfiguration.setCommunicationSpi(communicationSpi);
-
-        IgnitePredicate<DiscoveryEvent> locLsnr = evt -> {
-            logger.info("\n\n\tReceived event [evt=" + evt.name() + "] info : " + evt.shortDisplay() + "]\n");
-
-            return true; // Continue listening.
-        };
-
         Ignite ignite =  Ignition.start(igniteConfiguration);
-        IgniteEvents evts = ignite.events();
-        evts.localListen(locLsnr, EventType.EVT_NODE_JOINED, EventType.EVT_NODE_FAILED, EventType.EVT_NODE_LEFT,
-                EventType.EVT_NODE_SEGMENTED, EventType.EVT_CLIENT_NODE_DISCONNECTED, EventType.EVT_CLIENT_NODE_RECONNECTED);
 
         return ignite;
     }
